816f77d1a32ecd651fc07931ac406dbe7ceb13ea
nhertz <nhertz@nrao.edu>
2020-10-29 09:39:57 -0600

    Updated config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,4 @@ eval /Users/nhertz/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 export FZF_DEFAULT_OPTS="--multi --marker='+ ' --tabstop=4 --preview='bat -n --theme=\"1337\" --color always --decorations always {}'"
-
 starship init fish | source
